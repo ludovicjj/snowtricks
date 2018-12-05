56ae0ebd784df8624bc173bea7d6cd1326592a4a
ludovicjj <jahanlud@gmail.com>
2018-12-05 18:44:28 +0100

    Display Single trick

--- src/Controller/ShowTrickController.php
+++ src/Controller/ShowTrickController.php
@@ -2,8 +2,55 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Twig\Environment;
 
 class ShowTrickController
 {
+    /**
+     * @var TrickRepository
+     */
+    private $trickRepository;
 
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(
+        TrickRepository $trickRepository,
+        Environment $twig
+    )
+    {
+        $this->trickRepository = $trickRepository;
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("figure/{slug}", name="show_trick")
+     *
+     * @param Request $request
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function show(Request $request)
+    {
+        $trick = $this->trickRepository->findOneBy(['slug' => $request->attributes->get('slug')]);
+
+        if (!$trick) {
+            throw new NotFoundHttpException('Aucune figure ne correspond aux données reçu');
+        }
+
+        return new Response(
+            $this->twig->render('App/CRUD/show.html.twig', [
+                'trick' => $trick,
+            ])
+        );
+    }
 }
