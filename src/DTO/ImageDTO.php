95fa550d6e5ffa66e15a7858dfc3919fefd44c27
ludovicjj <jahanlud@gmail.com>
2018-12-08 20:02:20 +0100

    ImageDTO

--- src/DTO/ImageDTO.php
+++ src/DTO/ImageDTO.php
@@ -2,20 +2,21 @@
 
 namespace App\DTO;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ImageDTO
 {
     /**
-     * @var \SplFileInfo
+     * @var UploadedFile
      */
     public $file;
 
     /**
      * ImageDTO constructor.
-     * @param \SplFileInfo|null $file
+     * @param UploadedFile|null $file
      */
     public function __construct(
-        \SplFileInfo $file = null
+        UploadedFile $file = null
     )
     {
         $this->file = $file;
